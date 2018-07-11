d07061b4b6865e4d0aed4e44dc445c7f726967ca
Applejan <31026930+Applejan@users.noreply.github.com>
2018-07-11 18:44:28 +0800

    Update is.go

--- is.go
+++ is.go
@@ -1,9 +1,38 @@
+type sum struct {
+sum int
+}
+
+var a *sum
+
 func resize (src string,status chan int){
 f,err:=imaging.Open(src)
 if err!=nil{
 log.Fatal("File open fail,quiting!")
 }
 //f=imaging.Resize()
-c<-1
+status<-1
 }
-func distribut(sl [])
+
+
+
+
+fun main (){
+//Init start flag
+  status:=make(chan int, cpus)
+  for i:=0,i<cpus,i++{
+    status<-1
+  }
+  //To get filw list
+  var files []string
+for i,v :=range files {
+  distribut (v,a,status)
+}
+  
+}
+
+func distribut(file string,id *sum,status chan int){
+<-status
+  sum.sum+=status
+  fmt.Println("Current file ID is ",sum.sum)
+  go resize(file,status)
+} 
